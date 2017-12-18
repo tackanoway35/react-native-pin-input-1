22e75ec584cbc0dc7f7863f5ab9d0da203fc7065
Bùi Ngọc Lễ <buingocle.vp@gmail.com>
2017-12-18 15:08:39 +0700

    update style

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /**
  * @flow
  * */
-import React, {Component} from 'react';
-import {View, StyleSheet, TextInput} from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet, TextInput } from 'react-native';
 import Immutable from 'immutable';
-type P ={
+type P = {
     keyboardType?: string,
     placeholder?: string,
     autoFocus: boolean,
@@ -12,10 +12,10 @@ type P ={
     onPinCompleted: (string) => void,
     onPinsCompleted: (Array<string>) => void,
 }
-type S={
+type S = {
 
 }
-export default class PinInput extends Component<void,P,S> {
+export default class PinInput extends Component<void, P, S> {
     pinItemStyle: any;
     onPinCompleted: Function;
     pinItemProps: {};
@@ -52,7 +52,7 @@ export default class PinInput extends Component<void,P,S> {
                 throw new Error(`pin length is not equal ${this.pinLength}`)
             }
             let p = (this.props.placeholder || ' ');
-            this.setState({pins: Array.from(pin).map(v => v && v !== p ? v : null)});
+            this.setState({ pins: Array.from(pin).map(v => v && v !== p ? v : null) });
             if (this.props.autoFocus) {
                 this.focusPin(this.pinLength - 1)
             }
@@ -64,7 +64,7 @@ export default class PinInput extends Component<void,P,S> {
         if (pins.length !== this.pinLength) {
             throw new Error(`pin length is not equal ${this.pinLength}`)
         }
-        this.setState({pins: pins});
+        this.setState({ pins: pins });
         if (this.props.autoFocus) {
             this.focusPin(this.pinLength - 1)
         }
@@ -89,16 +89,16 @@ export default class PinInput extends Component<void,P,S> {
             this.blurPin(i)
         }
         let pins = Array(this.pinLength).fill(null);
-        this.setState({pins: Immutable.List(pins).toArray()});
+        this.setState({ pins: Immutable.List(pins).toArray() });
         if (this.props.autoFocus) {
-            this.setState({pins: Immutable.List(pins).set(0, null).toArray()});
+            this.setState({ pins: Immutable.List(pins).set(0, null).toArray() });
             this.focusPin(0)
         }
     }
 
     async onPinItemChanged(i, t) {
         let pins = Immutable.List(this.state.pins).set(i, t).toArray();
-        await this.setStateAsync({pins: pins});
+        await this.setStateAsync({ pins: pins });
         if (!t) {
             return
         }
@@ -133,29 +133,44 @@ export default class PinInput extends Component<void,P,S> {
     }
 
     render() {
+        let numberTextInput = this.props.pinLength;
         return (
-            <View style={{flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center'}}>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center' }}>
                 {
                     this.state.pins.map((v, i) => {
+                        let styleMiddlePinCode = {};
+                        if (numberTextInput % 2 === 0 && numberTextInput <= 8) {
+                            let indexMarginRight = (numberTextInput / 2) - 1;
+                            let indexMarginLeft = indexMarginRight + 1;
+                            if (i === indexMarginLeft) {
+                                styleMiddlePinCode = {
+                                    marginLeft: (this.props.distanceMiddlePinCode % 1 === 0 && this.props.distanceMiddlePinCode > 0 && typeof this.props.distanceMiddlePinCode === 'number') ? this.props.distanceMiddlePinCode / 2 : 10
+                                }
+                            } else if (i === indexMarginRight) {
+                                styleMiddlePinCode = {
+                                    marginRight: (this.props.distanceMiddlePinCode % 1 === 0 && this.props.distanceMiddlePinCode > 0 && typeof this.props.distanceMiddlePinCode === 'number') ? this.props.distanceMiddlePinCode / 2 : 10
+                                }
+                            }
+                        }
                         return (
                             <TextInput
                                 selectionColor={'transparent'}
                                 key={"pin_" + i}
                                 ref={`pin_${i}`}
                                 autoFocus={this.props.autoFocus && i === 0}
-                                style={StyleSheet.flatten([styles.pinItem, this.props.pinItemStyle])}
+                                style={StyleSheet.flatten([styles.pinItem, this.props.pinItemStyle, styleMiddlePinCode])}
                                 placeholder={this.props.placeholder || '_'}
                                 enablesReturnKeyAutomatically={true}
-                                keyboardType={(this.props.pinItemProps||{}).keyboardType || 'default'}
+                                keyboardType={(this.props.pinItemProps || {}).keyboardType || 'default'}
                                 returnKeyType={(this.props.pinItemProps || {}).returnKeyType || 'default'}
-                                secureTextEntry={(v && (this.props.pinItemProps || {}).secureTextEntry ) || false}
+                                secureTextEntry={(v && (this.props.pinItemProps || {}).secureTextEntry) || false}
                                 maxLength={1}
                                 onFocus={async (e) => await this.onPinFocus(i)}
                                 onChangeText={async (t) => {
                                     await this.onPinItemChanged(i, t);
                                 }}
                                 onKeyPress={(e) => this.onPinKeyPress(e, i)}
-                                value={this.state.pins[i]}/>
+                                value={this.state.pins[i]} />
                         )
                     })
                 }
@@ -177,7 +192,7 @@ export default class PinInput extends Component<void,P,S> {
                 }
             }
         } else {
-            await this.setStateAsync({pins: Immutable.List(this.state.pins).set(i, null).toArray()})
+            await this.setStateAsync({ pins: Immutable.List(this.state.pins).set(i, null).toArray() })
             this.setState({
                 isBackSpace: false
             })
